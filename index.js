2f457af28d28c6f3ee58b619b7ca7986d1b8084c
Brian Kimball <brian.kimball@bullhorn.com>
2022-01-20 10:56:50 -0500

    v0.0.2

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-module.exports = {
-  printWidth: 120,
-  semi: false,
-  singleQuote: true,
-  bracketSpacing: false,
-  trailingComma: 'none',
-  arrowParens: 'avoid'
+{
+  "singleQuote": true,
+  "useTabs": false,
+  "semi": true,
+  "bracketSpacing": true,
+  "printWidth": 140,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "arrowParens": "always",
+  "endOfLine": "lf",
+  "overrides": [
+    {
+      "files": "*.scss",
+      "options": {
+        "singleQuote": false
+      }
+    }
+  ]
 }
