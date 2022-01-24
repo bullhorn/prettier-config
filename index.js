d4a24e4e5fa386c85c5c6dda5c91c2c4fb4e7eaa
Brian Kimball <brian.kimball@bullhorn.com>
2022-01-24 13:29:29 -0500

    fix: js file needs to export parms :facepalm:

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-{
-  "singleQuote": true,
-  "useTabs": false,
-  "semi": true,
-  "bracketSpacing": true,
-  "printWidth": 140,
-  "tabWidth": 2,
-  "trailingComma": "all",
-  "arrowParens": "always",
-  "endOfLine": "lf",
-  "overrides": [
+module.exports = {
+  singleQuote: true,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  printWidth: 140,
+  tabWidth: 2,
+  trailingComma: "all",
+  arrowParens: "always",
+  endOfLine: "lf",
+  overrides: [
     {
-      "files": "*.scss",
-      "options": {
-        "singleQuote": false
-      }
-    }
-  ]
-}
+      files: "*.scss",
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+};
